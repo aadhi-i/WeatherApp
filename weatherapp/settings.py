ea3582e98cbe2d32693a86b0f78eaf612b077f63
Adithya S Nair <adithyasnair25@gmail.com>
2024-09-10 23:06:54 +0530

    deployment completed

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x@mumi8p5ogvq-zh+)l_7x@dh2r7vrs9el*k@k=*b6y+d=#70c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
